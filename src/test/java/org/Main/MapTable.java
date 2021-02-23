f2b6de785da403b831d02c449a8ba256de5f0dd9
Manish Sehgal <manish0718@users.noreply.github.com>
2021-02-24 00:56:03 +0530

    Update MapTable.java

--- src/test/java/org/Main/MapTable.java
+++ src/test/java/org/Main/MapTable.java
@@ -75,12 +75,12 @@ public class MapTableTest {
             e.printStackTrace();
         }
     }
-    
+
     /**
      * This method test whether the countries present in a map are different than expected.
      * If the countries names are different then expected than this test will get pass.
      */
-    
+
 	@Test
     public void testCountryNameNot()
     {
@@ -90,6 +90,49 @@ public class MapTableTest {
         	ArrayList<String> l_ans = new ArrayList<>(Arrays.asList("d","d","e","f","g","y"));
         	
         	assertNotEquals(l_ans,d_map.countryList(d_file));
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * This method test whether the continents and their control value present in a map is same as expected or not.
+     */
+
+	@Test
+    public void testContinentAndValue()
+    {
+        File d_file = new File(".\\src\\Main\\test\\h.map");
+        MapTable d_map= new MapTable();
+        try{
+        	HashMap<String,Integer> l_contval = new HashMap<>(); 
+        	l_contval.put("a", 5);
+        	l_contval.put("b", 5);
+        	
+        	assertEquals(l_contval,d_map.continentandvalue(d_file));
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * This method test whether the continents and their control value present in a map are different than expected.
+     * If the countries names with their control values are different then expected than this test will get pass.
+     */
+
+    @Test
+    public void testContinentAndValueNot()
+    {
+        File d_file = new File(".\\src\\Main\\test\\h.map");
+        MapTable d_map= new MapTable();
+        try{
+        	HashMap<String,Integer> l_contval = new HashMap<>(); 
+        	l_contval.put("b", 5);
+        	l_contval.put("b", 5);
+        	
+        	assertNotEquals(l_contval,d_map.continentandvalue(d_file));
         }catch (Exception e)
         {
             e.printStackTrace();
