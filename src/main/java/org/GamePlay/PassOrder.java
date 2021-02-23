f9b1167572a64ab4b21453c3c59f6f97f4976177
Manish Sehgal <manish0718@users.noreply.github.com>
2021-02-24 01:24:43 +0530

    Update PassOrder.java

--- src/main/java/org/GamePlay/PassOrder.java
+++ src/main/java/org/GamePlay/PassOrder.java
@@ -1,5 +1,10 @@
 package org.GamePlay;
 
+/**
+ * This class extends the Order class and called when player want to pass and don't want to give any order.
+ * This class just pass the order so as the counter will not ask the same player to order.
+ */
+
 public class PassOrder extends Order {
     Player P;
     Country COU;
@@ -11,3 +16,4 @@ public class PassOrder extends Order {
     @Override
     void Execute(Player p) {}
 }
+
