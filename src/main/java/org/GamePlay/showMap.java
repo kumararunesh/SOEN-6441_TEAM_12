92251f05276f3ca2d507c82f96daf92048aaeedb
Nitpreet Taneja <32680124+Nitpreet@users.noreply.github.com>
2021-02-23 22:40:31 +0530

    ShowMap file with Coding Conventions

--- src/main/java/org/GamePlay/showMap.java
+++ src/main/java/org/GamePlay/showMap.java
@@ -1,105 +1,106 @@
 package org.GamePlay;
 
-import org.Main.MapTable;
-
-import java.io.File;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.concurrent.ConcurrentHashMap;
 
+
 public class showMap {
-    static ConcurrentHashMap <String,Player> playersList = new ConcurrentHashMap<>();
-    static Country cou;
-    static String[][] adjacentNeighbours;
-    String[][] details;
-    public showMap(ConcurrentHashMap<String ,Player> playersList,Country cou)
+    static ConcurrentHashMap <String,Player> PLAYERS_LIST = new ConcurrentHashMap<>();
+    static Country COU;
+    static String[][] ADJACENTNEIGHBOURS;
+    String[][] DETAILS;
+
+    public showMap(ConcurrentHashMap<String ,Player> p_players_list, Country p_cou)
     {
-        this.cou = cou;
-        this.playersList = playersList;
+        this.COU = p_cou;
+        this.PLAYERS_LIST = p_players_list;
     }
 
-    public void addElement(int row, int col, String element)
+
+    public void l_addElement(int p_row, int p_col, String p_element)
     {
-        adjacentNeighbours[row][col]=element;
+        ADJACENTNEIGHBOURS[p_row][p_col]=p_element;
     }
+
 
     public void check() {
-        adjacentNeighbours = new String[cou.countriesList.size()+1][cou.countriesList.size()+1];
-        details = new String[cou.countriesList.size()][4];
-        ArrayList<String> allCountries = new ArrayList<>(cou.countriesList.keySet());
-        int row=0;
-        for(String countries: cou.countriesList.keySet())
+        ADJACENTNEIGHBOURS = new String[COU.COUNTRIESLIST.size()+1][COU.COUNTRIESLIST.size()+1];
+        DETAILS = new String[COU.COUNTRIESLIST.size()][4];
+        ArrayList<String> l_allCountries = new ArrayList<>(COU.COUNTRIESLIST.keySet());
+        int l_row=0;
+        for(String l_countries: COU.COUNTRIESLIST.keySet())
         {
-            details[row][0]= countries;
-            details[row][1]= cou.countriesList.get(countries).owner;
-            details[row][2]= cou.countriesList.get(countries).continent;
-            if(cou.countriesList.get(countries).numOfArmiesPlaced!=null) {
-                details[row][3] = cou.countriesList.get(countries).numOfArmiesPlaced.toString();
+            DETAILS[l_row][0]= l_countries;
+            DETAILS[l_row][1]= COU.COUNTRIESLIST.get(l_countries).OWNER;
+            DETAILS[l_row][2]= COU.COUNTRIESLIST.get(l_countries).CONTINENT;
+            if(COU.COUNTRIESLIST.get(l_countries).NUMOFARMIESPLACED!=null) {
+                DETAILS[l_row][3] = COU.COUNTRIESLIST.get(l_countries).NUMOFARMIESPLACED.toString();
             }else
             {
-                details[row][3] ="0";
+                DETAILS[l_row][3] ="0";
             }
-            row+=1;
+            l_row+=1;
         }
 
-        adjacentNeighbours[0][0]=" ";
-        row=1;
-        for(String country: cou.countriesList.keySet())
+        ADJACENTNEIGHBOURS[0][0]=" ";
+        l_row=1;
+        for(String l_country: COU.COUNTRIESLIST.keySet())
         {
-            adjacentNeighbours[0][row] = country;
-            row+=1;
+            ADJACENTNEIGHBOURS[0][l_row] = l_country;
+            l_row+=1;
         }
-        row=1;
-        for(String country:cou.countriesList.keySet())
+        l_row=1;
+        for(String l_country:COU.COUNTRIESLIST.keySet())
         {
-            adjacentNeighbours[row][0]=country;
-            row+=1;
+            ADJACENTNEIGHBOURS[l_row][0]=l_country;
+            l_row+=1;
         }
 
-        row=1;
+        l_row=1;
 
-        for(String country:cou.countriesList.keySet())
+        for(String l_country:COU.COUNTRIESLIST.keySet())
         {
 
-            for(String neighbour:cou.countriesList.get(country).neighbours)
+            for(String l_neighbour : COU.COUNTRIESLIST.get(l_country).NEIGHBOURS)
             {
-                int temp = allCountries.indexOf(neighbour);
-                addElement(row,temp+1,"X");
+                int l_temp = l_allCountries.indexOf(l_neighbour);
+                l_addElement(l_row,l_temp+1,"X");
             }
-            for(int  i=0;i<adjacentNeighbours[row].length;i++)
+            for(int l_index = 0; l_index< ADJACENTNEIGHBOURS[l_row].length; l_index++)
             {
-                if(adjacentNeighbours[row][i]==null)
+                if(ADJACENTNEIGHBOURS[l_row][l_index]==null)
                 {
-                    addElement(row,i,"O");
+                    l_addElement(l_row,l_index,"O");
                 }
             }
-            addElement(row,row,"X");
-            row+=1;
+            l_addElement(l_row,l_row,"X");
+            l_row+=1;
         }
 
         System.out.println("The details are: ");
-        row=0;
-        int col =0;
+        l_row=0;
+        int l_col =0;
         System.out.println("Country\t Owner\t Continent\t Armies");
-        for(row=0;row<details.length;row++)
+        for(l_row=0; l_row< DETAILS.length; l_row++)
         {
-            for(col=0;col<4;col++)
+            for(l_col=0;l_col<4;l_col++)
             {
-                System.out.print(details[row][col]+"\t");
+                System.out.print(DETAILS[l_row][l_col]+"\t");
             }
             System.out.println("");
         }
 
         System.out.println("Adjacency matrix: ");
-        row=0;
-        col=0;
-        for(row=0;row<adjacentNeighbours.length;row++)
+        l_row=0;
+        l_col=0;
+        for(l_row=0; l_row< ADJACENTNEIGHBOURS.length; l_row++)
         {
-            for(col=0;col<adjacentNeighbours[row].length;col++)
+            for(l_col=0; l_col< ADJACENTNEIGHBOURS[l_row].length; l_col++)
             {
-                System.out.print(adjacentNeighbours[row][col]+" ");
+                System.out.print(ADJACENTNEIGHBOURS[l_row][l_col]+" ");
             }
             System.out.println(" ");
         }
     }
 }
+
