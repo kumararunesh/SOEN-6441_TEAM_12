69a8fab091039a3f04ac2dfd49eff3a781251898
Richa Sharma <richa64044@gmail.com>
2021-02-24 15:48:48 -0500

    DeployOrder if armies to place is less than armies

--- src/main/java/org/GamePlay/DeployOrder.java
+++ src/main/java/org/GamePlay/DeployOrder.java
@@ -2,60 +2,57 @@ package org.GamePlay;
 
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ *Child class extending the Order Class , it deals with only deploy Order command
+ */
 public class DeployOrder extends Order {
-   String TYPEOFORDER = "DeployOrder" ;
-   String COUNTRYID;
-   Integer ARMIESTOPLACE;
-   Country COU;
-   public DeployOrder(String COUNTRYID, Integer ARMIESTOPLACE, Country COU) {
-      this.COUNTRYID = COUNTRYID;
-      this.ARMIESTOPLACE = ARMIESTOPLACE;
-      this.COU = COU;
+   String d_typeOfOrder = "DeployOrder";
+   String d_countryId;
+   Integer d_armiesToPlace;
+   Country d_cou;
+
+   /**
+    *Constructor to initialize the DeployOrder Object.
+    * @param p_countryId Country Name
+    * @param p_armiesToPlace No. of Armies to Deploy
+    * @param p_cou Country
+    */
+   public DeployOrder(String p_countryId, Integer p_armiesToPlace, Country p_cou) {
+      this.d_countryId = p_countryId;
+      this.d_armiesToPlace = p_armiesToPlace;
+      this.d_cou = p_cou;
    }
+
+   /**
+    * Implementation of Execute in Deploy Order
+    * @param p_p Player
+    */
    public void Execute(Player p_p)
    {
       int l_index=10000000;
-      for(int i = 0; i< p_p.OWNED.size(); i++)
+      for(int i = 0; i< p_p.d_owned.size(); i++)
       {
-         if(p_p.OWNED.get(i).COUNTRYID.equalsIgnoreCase(COUNTRYID))
+         if(p_p.d_owned.get(i).d_countryId.equalsIgnoreCase(d_countryId))
          {
             l_index= i;
          }
       }
       if (l_index!=10000000)
       {
-         if(ARMIESTOPLACE <= p_p.ARMIESNUM)
+         if(d_armiesToPlace <= p_p.d_armiesNum)
          {
-            p_p.ARMIESNUM = p_p.ARMIESNUM - ARMIESTOPLACE;
+            p_p.d_armiesNum = p_p.d_armiesNum - d_armiesToPlace;
 
-            COU.COUNTRIESLIST.get(COUNTRYID).NUMOFARMIESPLACED += ARMIESTOPLACE;
-            COU.COUNTRIESLIST.get(COUNTRYID).OWNER = p_p.NAME;
+            d_cou.COUNTRIESLIST.get(d_countryId).d_numOfArmiesPlaced += d_armiesToPlace;
+            d_cou.COUNTRIESLIST.get(d_countryId).d_owner = p_p.d_name;
 
-            System.out.print(p_p.NAME +" your order has been placed. Now your army count is "+ p_p.ARMIESNUM +". ");
-            System.out.print(ARMIESTOPLACE +" unit of armies are placed on "+ p_p.OWNED.get(l_index).COUNTRYID);
+            System.out.print(p_p.d_name +" your order has been placed. Now your army count is "+ p_p.d_armiesNum +". ");
+            System.out.print(d_armiesToPlace +" unit of armies are placed on "+ p_p.d_owned.get(l_index).d_countryId);
             System.out.println("");
             ConcurrentHashMap <String,Player> playerList = new ConcurrentHashMap<>();
-            playerList.put(p_p.NAME, p_p);
-            showMap map = new showMap(playerList, COU);
+            playerList.put(p_p.d_name, p_p);
+            showMap map = new showMap(playerList, d_cou);
          }
-         else
-         {
-//            p_p.OWNED.get(l_index).NUMOFARMIESPLACED = ARMIESTOPLACE;
-            COU.COUNTRIESLIST.get(COUNTRYID).NUMOFARMIESPLACED += p_p.ARMIESNUM;
-            COU.COUNTRIESLIST.get(COUNTRYID).OWNER = p_p.NAME;
-            System.out.println(p_p.NAME +" you only own "+ p_p.ARMIESNUM +" armies and all of them are now placed on "+p_p.OWNED.get(l_index).COUNTRYID);
 
-            p_p.ARMIESNUM = 0;
-         }
-      }
-      else
-      {
-         p_p.ARMIESNUM = p_p.ARMIESNUM - ARMIESTOPLACE;
-         if(p_p.ARMIESNUM <0)
-         {
-            p_p.ARMIESNUM =0;
-         }
-         System.out.println("You don't own this country. Penalty has been applied. Now your army count is "+ p_p.ARMIESNUM);
-      }
    }
-   }
+}
