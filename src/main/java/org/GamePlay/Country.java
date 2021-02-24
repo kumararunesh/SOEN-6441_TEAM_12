609448dfccfd4ff4dcc09aa4b364bd285a66c3fc
Nitpreet Taneja <32680124+Nitpreet@users.noreply.github.com>
2021-02-25 02:16:58 +0530

    Update Country.java
    Country File with JavaDoc

--- src/main/java/org/GamePlay/Country.java
+++ src/main/java/org/GamePlay/Country.java
@@ -3,24 +3,38 @@ package org.GamePlay;
 import java.util.ArrayList;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * Country Class containing all data for one country
+ */
 public class Country {
+    /**
+     * GLobal List of all countries
+     */
     static ConcurrentHashMap<String, Country> COUNTRIESLIST = new ConcurrentHashMap<>();
-    int NUMOFCOUNTRIESINCONTINENT;
-    String COUNTRYID;
-    Integer NUMOFARMIESPLACED=0;
-    ArrayList<String> NEIGHBOURS = new ArrayList();
-    String CONTINENT;
-    String OWNER;
-    int CONTINENTBONUS;
-    public Country(String COUNTRYID, String CONTINENT, int CONTINENTBONUS, int NUMOFCOUNTRIESINCONTINENT, ArrayList NEIGHBOURS)
-    {
-        this.COUNTRYID = COUNTRYID;
-        this.CONTINENT = CONTINENT;
-        this.CONTINENTBONUS = CONTINENTBONUS;
-        this.NUMOFCOUNTRIESINCONTINENT = NUMOFCOUNTRIESINCONTINENT;
-        this.NEIGHBOURS = NEIGHBOURS;
+
+    int d_numberOfCountriesInContinent;
+    String d_countryId;
+    Integer d_numOfArmiesPlaced = 0;
+    ArrayList<String> d_neighbours = new ArrayList();
+    String d_continent;
+    String d_owner;
+    int d_continentBonus;
+
+    /**
+     * Constructor to intialize the country object
+     *
+     * @param p_countryId                    Country Name
+     * @param p_continent                    Continent Name
+     * @param p_continentBonus               Continent Control Value
+     * @param p_numberOfCountriesInContinent number Of Countries In Continent
+     * @param p_neighbours                   list of neighbouring countries
+     */
+    public Country(String p_countryId, String p_continent, int p_continentBonus, int p_numberOfCountriesInContinent, ArrayList p_neighbours) {
+        this.d_countryId = p_countryId;
+        this.d_continent = p_continent;
+        this.d_continentBonus = p_continentBonus;
+        this.d_numberOfCountriesInContinent = p_numberOfCountriesInContinent;
+        this.d_neighbours = p_neighbours;
     }
-
-
-
+    
 }
