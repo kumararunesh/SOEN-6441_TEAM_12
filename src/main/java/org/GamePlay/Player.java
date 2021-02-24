714fdc274f0b96fc5c94b23030e9447bbbe747a3
kumararunesh <kumararunesh211994@gmail.com>
2021-02-25 02:01:22 +0530

    Update Player.java

--- src/main/java/org/GamePlay/Player.java
+++ src/main/java/org/GamePlay/Player.java
@@ -4,49 +4,64 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/**
+ *Player Class containing all the stats of the Player Class
+ */
 public class Player {
 
-    ArrayList<Country> OWNED = new ArrayList<Country>();
-    Queue<Order> ORDERS = new LinkedList<Order>();
-    Integer ARMIESNUM;
-    String NAME;
-    int CONTINENTVALUE; 
+    ArrayList<Country> d_owned = new ArrayList<Country>();
+    Queue<Order> d_orders = new LinkedList<Order>();// Queue only
+    Integer d_armiesNum;
+    String d_name;
+    int d_continentValue; // Continent Control Value ;
 
-    public Player(String NAME) {
-        this.NAME = NAME;
+    /**
+     * Parameterised Constructor to initilize Player Object by name .
+     * @param p_name  Player Name
+     */
+    public Player(String p_name) {
+        this.d_name = p_name;
     }
 
-    public String getNAME() {
-        return NAME;
+    /**
+     * getter for Player Name.
+     * @return Player Name
+     */
+    public String getD_name() {
+        return d_name;
     }
 
-    public ArrayList<Country> getOWNED() {
-        return OWNED;
+    /**
+     *Getter for list of countries owned by the player.
+     * @return list of countries owned by the player
+     */
+    public ArrayList<Country> getD_owned() {
+        return d_owned;
     }
-    
-     /**
+
+    /**
+     *setter for owned country by the player
+     * @param p_country country won by the player
+     */
+    public void setD_owned(Country p_country) {
+        this.d_owned.add(p_country);
+    }
+
+    /**
      *function to issue order .
      * @param p_order issue order object
      */
-    public void setOWNED(Country country) {
-        this.OWNED.add(country);
-    }
+   public void issue_order(Order p_order)
+   {
+       d_orders.add(p_order);
+   }
 
-      /**
+    /**
      * function to fetch next order.
      * @return Order Next Order to be executed
      */
    public Order next_order()
    {
         return this.d_orders.poll(); // FIFO for queue .
-   }
-   public void issue_order(Order order)
-   {
-       ORDERS.add(order);
-   }
-
-   public Order next_order()
-   {
-        return this.ORDERS.poll(); 
    }
 }
