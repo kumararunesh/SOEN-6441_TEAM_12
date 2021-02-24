d00d0c3d71ec89aea93802c2bddb958346a5c49e
kumararunesh <kumararunesh211994@gmail.com>
2021-02-25 02:00:34 +0530

    Update Player.java

--- src/main/java/org/GamePlay/Player.java
+++ src/main/java/org/GamePlay/Player.java
@@ -23,11 +23,23 @@ public class Player {
     public ArrayList<Country> getOWNED() {
         return OWNED;
     }
-
+    
+     /**
+     *function to issue order .
+     * @param p_order issue order object
+     */
     public void setOWNED(Country country) {
         this.OWNED.add(country);
     }
 
+      /**
+     * function to fetch next order.
+     * @return Order Next Order to be executed
+     */
+   public Order next_order()
+   {
+        return this.d_orders.poll(); // FIFO for queue .
+   }
    public void issue_order(Order order)
    {
        ORDERS.add(order);
