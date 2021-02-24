a5b79644965761a89946b54a67082d13b9b0d179
kumararunesh <kumararunesh211994@gmail.com>
2021-02-24 15:05:14 +0530

    Implementing Country Constructor

--- src/main/java/org/GamePlay/Country.java
+++ src/main/java/org/GamePlay/Country.java
@@ -12,5 +12,15 @@ public class Country {
     String CONTINENT;
     String OWNER;
     int CONTINENTBONUS;
-  
+    public Country(String COUNTRYID, String CONTINENT, int CONTINENTBONUS, int NUMOFCOUNTRIESINCONTINENT, ArrayList NEIGHBOURS)
+    {
+        this.COUNTRYID = COUNTRYID;
+        this.CONTINENT = CONTINENT;
+        this.CONTINENTBONUS = CONTINENTBONUS;
+        this.NUMOFCOUNTRIESINCONTINENT = NUMOFCOUNTRIESINCONTINENT;
+        this.NEIGHBOURS = NEIGHBOURS;
+    }
+
+
+
 }
