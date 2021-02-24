657d4297dd75f163709aeb5483e4a85213df12ec
Manish Sehgal <manish0718@users.noreply.github.com>
2021-02-24 15:45:55 +0530

    Update Order.java

--- src/main/java/org/GamePlay/Order.java
+++ src/main/java/org/GamePlay/Order.java
@@ -9,5 +9,5 @@ public abstract class Order {
      * This is the abstract method decleration which will execute the orders issued by each player.
      * @param p this is the player object, which will execute the correct orders from each individual player.
      */
-    abstract void Execute(Player p);
+    abstract void Execute(Player p_p);
 }
