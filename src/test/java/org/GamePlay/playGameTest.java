8a17c8ea3b2aa63aeff5e6e2b9261b9c9e204439
Nitpreet Taneja <32680124+Nitpreet@users.noreply.github.com>
2021-02-25 01:13:36 +0530

    Another test case for checking the armies alloted to the player

--- src/test/java/org/GamePlay/playGameTest.java
+++ src/test/java/org/GamePlay/playGameTest.java
@@ -38,10 +38,44 @@ public class playGameTest {
 
         d_playerList.put("Nitpreet",d_player);
         d_playGame = new playGame(d_playerList,d_cou);
-	 }
+
+
+
+        d_country11 = new Country("c","a",5,3,new ArrayList<>(Arrays.asList("d","e")));
+        d_country11.d_owner="xyz";
+
+        d_country21 = new Country("d","b",5,3,new ArrayList<>(Arrays.asList("c","e")));
+        d_country21.d_owner="xyz";
+
+        d_country31 = new Country("y","a",5,3,new ArrayList<>(Arrays.asList("d","e")));
+        d_country31.d_owner="xyz";
+
+        d_cou.COUNTRIESLIST.put("c",d_country11);
+        d_cou.COUNTRIESLIST.put("d",d_country21);
+        d_cou.COUNTRIESLIST.put("y",d_country31);
+
+        Player d_player11 = new Player("xyz");
+        d_player11.d_owned.add(d_country11);
+        d_player11.d_owned.add(d_country21);
+        d_player11.d_owned.add(d_country31);
+
+        d_playerList1.put("xyz",d_player11);
+        d_playGame1 = new playGame(d_playerList1,d_cou1);
+
+
+
+    }
 
     @org.junit.Test
     public void playGameLoop() {
 
         assertEquals(8,d_playGame.playGameLoop());
     }
+
+
+    @org.junit.Test
+    public void playGameLoop1() {
+
+        assertEquals(3,d_playGame1.playGameLoop());
+    }
+}
