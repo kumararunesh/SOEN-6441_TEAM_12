0c545d401f85c23051c526e90345a1576f3b3e10
kumararunesh <kumararunesh211994@gmail.com>
2021-02-24 15:03:32 +0530

    Country Variables

--- src/main/java/org/GamePlay/Country.java
+++ src/main/java/org/GamePlay/Country.java
@@ -5,7 +5,14 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class Country {
     static ConcurrentHashMap<String, Country> COUNTRIESLIST = new ConcurrentHashMap<>();
-
-
+    int NUMOFCOUNTRIESINCONTINENT;
+    String COUNTRYID;
+    Integer NUMOFARMIESPLACED=0;
+    ArrayList<String> NEIGHBOURS = new ArrayList();
+    String CONTINENT;
+    String OWNER;
+    int CONTINENTBONUS;
+    public Country(String COUNTRYID, String CONTINENT, int CONTINENTBONUS, int NUMOFCOUNTRIESINCONTINENT, ArrayList NEIGHBOURS)
+  
 
 }
