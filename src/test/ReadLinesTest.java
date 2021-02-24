b636a007fb54490ff0f23bc627432d5ab4874691
Richa Sharma <richa64044@gmail.com>
2021-02-24 14:47:37 -0500

    Test cases for ReadLines added

---
+++ src/test/ReadLinesTest.java
@@ -0,0 +1,38 @@
+package org.Main;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import java.io.File;
+
+
+/**
+ * This is test class which test different methods of ReadLines class.
+ */
+
+public class ReadLinesTest {
+
+    /**
+     * This method will test whether the number of border and country lines in the map is same as expected or not.
+     * If the border and country lines are same as expected then the test case will get pass.
+     */
+
+    @Test
+    public void TestCountryAndBorderLines()
+    {
+        ReadLines l_map = new ReadLines();
+        File l_file = new File(".\\src\\main\\resources\\maps\\h.map");
+        try {
+            assertEquals(7,l_map.countryandborderline(l_file));
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+
+
+}
