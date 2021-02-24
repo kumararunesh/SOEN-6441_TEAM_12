789995a3091150e3d6acb33c3d9aede3c4f5289c
kumararunesh <kumararunesh211994@gmail.com>
2021-02-24 15:02:12 +0530

    Country

---
+++ src/main/java/org/GamePlay/Country.java
@@ -0,0 +1,11 @@
+package org.GamePlay;
+
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Country {
+    static ConcurrentHashMap<String, Country> COUNTRIESLIST = new ConcurrentHashMap<>();
+
+
+
+}
