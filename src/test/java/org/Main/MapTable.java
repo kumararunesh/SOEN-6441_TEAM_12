4498f05fd04d6c95175f4b9378b6aad703be9fff
Manish Sehgal <manish0718@users.noreply.github.com>
2021-02-24 15:53:40 +0530

    Update MapTable.java

--- src/test/java/org/Main/MapTable.java
+++ src/test/java/org/Main/MapTable.java
@@ -25,12 +25,12 @@ public class MapTableTest {
     @Test
     public void testConitnentName()
     {
-        File d_file = new File(".\\src\\Main\\test\\h.map");
-        MapTable d_map= new MapTable();
+        File l_file = new File(".\\src\\Main\\test\\h.map");
+        MapTable l_map= new MapTable();
         try{
         	ArrayList<String> l_ans = new ArrayList<>(Arrays.asList("a","b"));
         	
-            assertEquals(l_ans,d_map.continentList(d_file));
+            assertEquals(l_ans,l_map.continentList(l_file));
         }catch (Exception e)
         {
             e.printStackTrace();
@@ -45,12 +45,12 @@ public class MapTableTest {
     @Test
     public void testContinentNameNot()
     {
-        File d_file = new File(".\\src\\Main\\test\\h.map");
-        MapTable d_map= new MapTable();
+        File l_file = new File(".\\src\\Main\\test\\h.map");
+        MapTable l_map= new MapTable();
         try{
         	ArrayList<String> l_ans = new ArrayList<>(Arrays.asList("b","b"));
         	
-        	assertNotEquals(l_ans,d_map.continentList(d_file));
+        	assertNotEquals(l_ans,l_map.continentList(l_file));
         }catch (Exception e)
         {
             e.printStackTrace();
@@ -64,12 +64,12 @@ public class MapTableTest {
     @Test
     public void testCountryName()
     {
-        File d_file = new File(".\\src\\Main\\test\\h.map");
-        MapTable d_map= new MapTable();
+        File l_file = new File(".\\src\\Main\\test\\h.map");
+        MapTable l_map= new MapTable();
         try{
         	ArrayList<String> l_ans = new ArrayList<>(Arrays.asList("c","d","e","f","g","y"));
         	
-        	assertEquals(l_ans,d_map.countryList(d_file));
+        	assertEquals(l_ans,l_map.countryList(l_file));
         }catch (Exception e)
         {
             e.printStackTrace();
@@ -84,12 +84,12 @@ public class MapTableTest {
 	@Test
     public void testCountryNameNot()
     {
-        File d_file = new File(".\\src\\Main\\test\\h.map");
-        MapTable d_map= new MapTable();
+        File l_file = new File(".\\src\\Main\\test\\h.map");
+        MapTable l_map= new MapTable();
         try{
         	ArrayList<String> l_ans = new ArrayList<>(Arrays.asList("d","d","e","f","g","y"));
         	
-        	assertNotEquals(l_ans,d_map.countryList(d_file));
+        	assertNotEquals(l_ans,l_map.countryList(l_file));
         }catch (Exception e)
         {
             e.printStackTrace();
@@ -103,14 +103,14 @@ public class MapTableTest {
 	@Test
     public void testContinentAndValue()
     {
-        File d_file = new File(".\\src\\Main\\test\\h.map");
-        MapTable d_map= new MapTable();
+        File l_file = new File(".\\src\\Main\\test\\h.map");
+        MapTable l_map= new MapTable();
         try{
         	HashMap<String,Integer> l_contval = new HashMap<>(); 
         	l_contval.put("a", 5);
         	l_contval.put("b", 5);
         	
-        	assertEquals(l_contval,d_map.continentandvalue(d_file));
+        	assertEquals(l_contval,l_map.continentandvalue(l_file));
         }catch (Exception e)
         {
             e.printStackTrace();
@@ -125,16 +125,274 @@ public class MapTableTest {
     @Test
     public void testContinentAndValueNot()
     {
-        File d_file = new File(".\\src\\Main\\test\\h.map");
-        MapTable d_map= new MapTable();
+        File l_file = new File(".\\src\\Main\\test\\h.map");
+        MapTable l_map= new MapTable();
         try{
         	HashMap<String,Integer> l_contval = new HashMap<>(); 
         	l_contval.put("b", 5);
         	l_contval.put("b", 5);
         	
-        	assertNotEquals(l_contval,d_map.continentandvalue(d_file));
+        	assertNotEquals(l_contval,l_map.continentandvalue(l_file));
         }catch (Exception e)
         {
             e.printStackTrace();
         }
     }
+
+    /**
+     * This method test whether the countries and their unique key present in a map is same as expected or not.
+     */
+	
+	@Test
+    public void testCountryAndItsKey()
+    {
+        File l_file = new File(".\\src\\Main\\test\\h.map");
+        MapTable l_map= new MapTable();
+        try{
+        	HashMap<Integer,String> l_contkey = new HashMap<>(); 
+        	l_contkey.put(1,"c");
+        	l_contkey.put(2,"d");
+        	l_contkey.put(3,"e");
+        	l_contkey.put(4,"f");
+        	l_contkey.put(5,"g");
+        	l_contkey.put(6,"y");
+        	
+        	assertEquals(l_contkey,l_map.countryanditskey(l_file));
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * This method test whether the countries and their unique key present in a map are different than expected.
+     * If the countries names with their unique key are different then expected than this test will get pass.
+     */
+
+	@Test
+    public void testCountryAndItsKeyNot()
+    {
+        File l_file = new File(".\\src\\Main\\test\\h.map");
+        MapTable l_map= new MapTable();
+        try{
+        	HashMap<Integer,String> l_contkey = new HashMap<>(); 
+        	l_contkey.put(2,"c");
+        	l_contkey.put(2,"d");
+        	l_contkey.put(3,"c");
+        	l_contkey.put(4,"f");
+        	l_contkey.put(5,"g");
+        	l_contkey.put(6,"y");
+        	
+        	assertNotEquals(l_contkey,l_map.countryanditskey(l_file));
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * This method test whether the countries and the continent to which they belong present in a map is same as expected or not.
+     */
+	
+	@Test
+    public void testCountryAndContinent()
+    {
+        File l_file = new File(".\\src\\Main\\test\\h.map");
+        MapTable l_map= new MapTable();
+        try{
+        	HashMap<String,String> l_countcont = new HashMap<>(); 
+        	l_countcont.put("c","a");
+        	l_countcont.put("d","a");
+        	l_countcont.put("e","b");
+        	l_countcont.put("f","b");
+        	l_countcont.put("g","b");
+        	l_countcont.put("y","a");
+        	
+        	assertEquals(l_countcont,l_map.countryanditscontinent(l_file));
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * This method test whether the countries and the continent to which they belong present in a map are different than expected.
+     * If the countries and the continent to which they belong are different then expected than this test will get pass.
+     */
+	
+	@Test
+    public void testCountryAndContinentNot()
+    {
+        File l_file = new File(".\\src\\Main\\test\\h.map");
+        MapTable l_map= new MapTable();
+        try{
+        	HashMap<String,String> l_countcont = new HashMap<>(); 
+        	l_countcont.put("a","a");
+        	l_countcont.put("d","a");
+        	l_countcont.put("e","c");
+        	l_countcont.put("f","b");
+        	l_countcont.put("g","b");
+        	l_countcont.put("y","a");
+        	
+        	assertNotEquals(l_countcont,l_map.countryanditscontinent(l_file));
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * This method test whether the countries and their neighbors present in a map is same as expected or not.
+     */
+	
+	@Test
+    public void testCountryAndItsNeighbors()
+    {
+        File l_file = new File(".\\src\\Main\\test\\h.map");
+        MapTable l_map= new MapTable();
+        try{
+        	HashMap<String,ArrayList> l_countryNeigh = new HashMap<>(); 
+        	l_countryNeigh.put("c",new ArrayList(Arrays.asList("d","e","f","g","y")));
+        	l_countryNeigh.put("d",new ArrayList(Arrays.asList("c","e")));
+        	l_countryNeigh.put("e",new ArrayList(Arrays.asList("f","c","d")));
+        	l_countryNeigh.put("f",new ArrayList(Arrays.asList("d","c")));
+        	l_countryNeigh.put("g",new ArrayList(Arrays.asList("d","c")));
+        	l_countryNeigh.put("y",new ArrayList(Arrays.asList("d","e")));
+        	
+        	assertEquals(l_countryNeigh,l_map.countryanditsneighbours(l_file));
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * This method test whether the countries and their neighbors present in a map are different than expected.
+     * If the countries and their neighbors are different then expected than this test will get pass.
+     */
+
+	@Test
+    public void testCountryAndItsNeighborsNot()
+    {
+        File l_file = new File(".\\src\\Main\\test\\h.map");
+        MapTable l_map= new MapTable();
+        try{
+        	HashMap<String,ArrayList> l_countryNeigh = new HashMap<>(); 
+        	l_countryNeigh.put("c",new ArrayList(Arrays.asList("e","e","f","g","y")));
+        	l_countryNeigh.put("d",new ArrayList(Arrays.asList("c","e")));
+        	l_countryNeigh.put("e",new ArrayList(Arrays.asList("a","c","d")));
+        	l_countryNeigh.put("f",new ArrayList(Arrays.asList("d","c")));
+        	l_countryNeigh.put("g",new ArrayList(Arrays.asList("d","c")));
+        	l_countryNeigh.put("y",new ArrayList(Arrays.asList("d","e")));
+        	
+        	assertNotEquals(l_countryNeigh,l_map.countryanditsneighbours(l_file));
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * This method test whether the countries and the continent's unique key to which that country belongs present in a map is same as expected or not.
+     */
+
+	@Test
+    public void testCountryAndItsUniqueContinent()
+    {
+        File l_file = new File(".\\src\\Main\\test\\h.map");
+        MapTable l_map= new MapTable();
+        try{
+        	HashMap<String,Integer> l_countcontkey = new HashMap<>(); 
+        	l_countcontkey.put("c", 1);
+        	l_countcontkey.put("d", 1);
+        	l_countcontkey.put("e", 2);
+        	l_countcontkey.put("f", 2);
+        	l_countcontkey.put("g", 2);
+        	l_countcontkey.put("y", 1);
+        	
+        	assertEquals(l_countcontkey,l_map.countryanditsuniquecontinent(l_file));
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * This method test whether countries and the continent's unique key to which that country belongs present in a map are different than expected.
+     * If the countries and the continent's unique key to which that country belongs are different then expected than this test will get pass.
+     */
+
+    @Test
+    public void testCountryAndItsUniqueContinentNot()
+    {
+        File l_file = new File(".\\src\\Main\\test\\h.map");
+        MapTable l_map= new MapTable();
+        try{
+        	HashMap<String,Integer> l_countcontkey = new HashMap<>(); 
+        	l_countcontkey.put("q", 1);
+        	l_countcontkey.put("d", 1);
+        	l_countcontkey.put("e", 2);
+        	l_countcontkey.put("v", 2);
+        	l_countcontkey.put("g", 2);
+        	l_countcontkey.put("y", 1);
+        	
+        	assertNotEquals(l_countcontkey,l_map.countryanditsuniquecontinent(l_file));
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * This method test whether the countries and their unique key present in a map is same as expected or not.
+     */
+
+    @Test
+    public void testUniqueKeyAndItsCountry()
+    {
+        File l_file = new File(".\\src\\Main\\test\\h.map");
+        MapTable l_map= new MapTable();
+        try{
+        	HashMap<String,Integer> l_keycount = new HashMap<>(); 
+        	l_keycount.put("c", 1);
+        	l_keycount.put("d", 2);
+        	l_keycount.put("e", 3);
+        	l_keycount.put("f", 4);
+        	l_keycount.put("g", 5);
+        	l_keycount.put("y", 6);
+        	
+        	assertEquals(l_keycount,l_map.uniqueKeyanditscountry(l_file));
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * This method test whether countries and their unique key present in a map are different than expected.
+     * If the countries and their unique key are different then expected than this test will get pass.
+     */
+
+
+    @Test
+    public void testUniqueKeyAndItsCountryNot()
+    {
+        File l_file = new File(".\\src\\Main\\test\\h.map");
+        MapTable l_map= new MapTable();
+        try{
+        	HashMap<String,Integer> l_keycount = new HashMap<>(); 
+        	l_keycount.put("c", 1);
+        	l_keycount.put("d", 2);
+        	l_keycount.put("e", 3);
+        	l_keycount.put("d", 4);
+        	l_keycount.put("g", 7);
+        	l_keycount.put("y", 6);
+        	
+        	assertNotEquals(l_keycount,l_map.uniqueKeyanditscountry(l_file));
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+	
+}
