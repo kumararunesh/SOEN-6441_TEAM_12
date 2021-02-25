1eef446eca1da0d110bcd593ccd81604587710a6
Manjit110 <49787726+Manjit110@users.noreply.github.com>
2021-02-25 18:50:42 +0530

    Update MapValidation.java

--- src/main/java/org/Main/MapValidation.java
+++ src/main/java/org/Main/MapValidation.java
@@ -1,5 +1,9 @@
 package org.Main;
 import java.io.File;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -9,10 +13,7 @@ import java.util.Map;
  * If the map is validated, then the file will get saved else it will show that the file in not correct.
  */
 public class MapValidation {
-    /**
-     * Flag to check whether map is valid or not.
-     */
-    public int d_final_flag = 0;
+
     /**
      * This method will get the details of the map after user edit some changes in the map and then it will validate the map on some conditions.
      * If the map is validated, then the file will get saved else it will show that the file in not correct.
@@ -29,12 +30,12 @@ public class MapValidation {
      */
 
     public void mapValidate(File p_file,ArrayList<String> p_countries,ArrayList<String> p_continent,HashMap<String, Integer> p_cont_val, HashMap<Integer, String> p_country_key,HashMap<String, String> p_country_cont,HashMap<String, ArrayList> p_country_neighbour,HashMap<String, Integer> p_country_cont_key,HashMap<String, Integer> p_cont_unique_key) throws Exception {
-
+        int l_final_flag = 0;
         for (int l_i = 0; l_i < p_countries.size(); l_i++) {
             for(int l_j=l_i+1; l_j < p_countries.size()-1;l_j++){
                 if(p_countries.get(l_i).equalsIgnoreCase(p_countries.get(l_j))){
                     System.out.println("Countries cannot be of same name");
-                    d_final_flag = 1;
+                    l_final_flag = 1;
                     break;
                 }
             }
@@ -42,14 +43,14 @@ public class MapValidation {
         for (Map.Entry<Integer, String> l_entry : p_country_key.entrySet()) {
             if(l_entry.getKey() <= 0){
                 System.out.println("Country's unique key cannot be negaive");
-                d_final_flag = 1;
+                l_final_flag = 1;
             }
         }
         for (int l_i = 0; l_i < p_continent.size(); l_i++) {
             for(int l_j=l_i+1; l_j < p_continent.size()-1;l_j++){
                 if(p_continent.get(l_i).equalsIgnoreCase(p_continent.get(l_j))){
                     System.out.println(" Continents cannot be of same name ");
-                    d_final_flag = 1;
+                    l_final_flag = 1;
                     break;
                 }
             }
@@ -57,7 +58,7 @@ public class MapValidation {
         for (Map.Entry<String, Integer> l_entry : p_cont_val.entrySet()) {
             if(l_entry.getValue() < 0){
                 System.out.println("Continent's Control value cannot be negative");
-                d_final_flag = 1;
+                l_final_flag = 1;
                 break;
             }
         }
@@ -66,7 +67,7 @@ public class MapValidation {
             ArrayList l_value = l_map_element.getValue();
             for(int l_i=0;l_i<l_value.size();l_i++){
                 if(l_value.get(l_i).equals("DNE")){
-                    d_final_flag = 1;
+                    l_final_flag = 1;
                 }
             }
         }
@@ -101,29 +102,33 @@ public class MapValidation {
                 continue;
             }
             else{
-                d_final_flag =1;
+                l_final_flag=1;
                 break;
             }
         }
-        if(d_final_flag ==1)
+        if(l_final_flag==1)
         {
             String l_a = "Map is Incorrect";
             System.out.println("Map is Incorrect");
         }
-        else if(d_final_flag ==0)
+        else if(l_final_flag==0)
         {
             EditMap l_save  = new EditMap();
             File l_file = l_save.fileCreation(p_file, p_cont_unique_key, p_cont_val, p_country_cont_key, p_country_neighbour, p_country_key);
             GraphConnected l_validate = new GraphConnected(l_file);
+            ContinentsConnected l_obj = new ContinentsConnected(l_file);
             boolean l_check = l_validate.ifGraphConnected();
-            if(l_check==true){
+            boolean l_sub = l_validate.ContinentsCheck();
+            boolean l_one = l_obj.ifContinentsConnected();
+
+            if(l_check==true && l_sub == true && l_one==true ){
                 System.out.println("**************************************");
                 System.out.println("Graph is Connected");
                 System.out.println("**************************************");
                 System.out.println("");
 
                 System.out.println("**************************************");
-                System.out.println("Map is validated");
+                System.out.println("Map is validated and created successfully");
                 System.out.println("**************************************");
             }
             else{
@@ -136,8 +141,12 @@ public class MapValidation {
                 System.out.println("**************************************");
                 System.out.println("Map is Incorrect");
                 System.out.println("**************************************");
+
                 Main.menu();
             }
+
         }
+
+
     }
 }
