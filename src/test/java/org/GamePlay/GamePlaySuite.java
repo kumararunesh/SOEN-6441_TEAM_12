e056eaf34ffa1f25ce700d3d239084e7c59aa045
Nitpreet Taneja <32680124+Nitpreet@users.noreply.github.com>
2021-02-25 17:55:13 +0530

    Suite class to run all the classes.

---
+++ src/test/java/org/GamePlay/GamePlaySuite.java
@@ -0,0 +1,12 @@
+package org.GamePlay;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Class for generating the Suite for the GamePlay
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({DeployOrderTest.class,playGameTest.class})
+public class GamePlaySuite {
+}
