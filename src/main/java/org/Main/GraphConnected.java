4a31a6acbf5236c0fe7b3ccdba8157aeeeeab835
Nitpreet Taneja <32680124+Nitpreet@users.noreply.github.com>
2021-02-25 16:38:24 +0530

    Update GraphConnected.java
    Updated with coding Conventions

--- src/main/java/org/Main/GraphConnected.java
+++ src/main/java/org/Main/GraphConnected.java
@@ -10,27 +10,54 @@ import java.util.HashMap;
 
 public class GraphConnected{
     static File FILE;
-    MapTable BORDERS = new MapTable();
-    HashMap<Integer,ArrayList> COUNTRYNEIGH;
-    List<Integer> p_RealNodes;
-    Stack<Integer> STACK;
-    ArrayList<Integer> CONNECTIONS;
-    int MAX;
-    boolean[] VISITED;
+    MapTable d_borders = new MapTable();
+    HashMap<Integer,ArrayList> d_countryNeigh;
+    List<Integer> d_realNodes;
+    List<Integer> d_country_index;
+    Stack<Integer> d_stack;
+    ArrayList<Integer> d_connections;
+    int d_max;
+    boolean[] d_visited;
+    boolean[] d_scan;
 
     /**
      * Simple Constructor throwing exception.
-     * @param p_file is the File path
+     * @param p_file is the object of the file.
      * @throws Exception whether the file exists or not.
      */
     public GraphConnected(File p_file) throws Exception {
         this.FILE = p_file;
-        COUNTRYNEIGH =BORDERS.CountryAndItsNeighbours(FILE);
-        p_RealNodes = BORDERS.max(FILE);
-        STACK = new Stack<>();
-        CONNECTIONS = new ArrayList<>();
-        MAX = Collections.max(p_RealNodes,null);
-        VISITED = new boolean[MAX+1];
+        d_countryNeigh = d_borders.CountryAndItsNeighbours(FILE);
+        d_country_index = d_borders.list(FILE);
+        d_realNodes = d_borders.max(FILE);
+        d_stack = new Stack<>();
+        d_connections = new ArrayList<>();
+        d_max = Collections.max(d_realNodes,null);
+        d_visited = new boolean[d_max +1];
+        d_scan = new boolean[d_max + 1];
+    }
+
+    /**
+     * This function is modifying the Hashmap of the .map file values, to its relevant form.
+     * @return Hashmap which has each country unique id and in its values is..
+     * Arraylist of neighbouring countries unique id's.
+     */
+    public HashMap<Integer,ArrayList> complete(){
+        ArrayList<Integer> l_empty = new ArrayList<>();
+
+        d_scan[0] = true;
+        for (int l_i = 0; l_i<d_country_index.size(); l_i++){
+            d_scan[d_country_index.get(l_i)]= true;
+        }
+        for (int l_j = 0; l_j<d_realNodes.size(); l_j++){
+            if(d_scan[d_realNodes.get(l_j)]==false){
+                int l_note = d_realNodes.get(l_j);
+                d_countryNeigh.put(l_note,l_empty);
+            }
+        }
+
+        return  d_countryNeigh;
+
     }
 
     /**
@@ -39,39 +66,26 @@ public class GraphConnected{
      * @param p_value variable p_value is integer value which determines from where the graph traversing should start.
      */
     public void dfs(int p_value) {
-
-        STACK.push(p_value);
-        VISITED[p_value] = true;
-        int count = 0;
-
-
+        d_stack.push(p_value);
+        d_visited[p_value] = true;
+        complete();
+        int l_count = 0;
         while (true) {
-
-            if(STACK.isEmpty() == true){
+            if(d_stack.isEmpty() == true){
                 break;
             }
-
-            Integer l_node = STACK.pop();
-
-
-            CONNECTIONS = COUNTRYNEIGH.get(l_node);
-
-            if(!CONNECTIONS.isEmpty()){
-                for (Integer neighbour : CONNECTIONS) {
-
-                    if (!VISITED[neighbour]) {
-                        STACK.push(neighbour);
-                        VISITED[neighbour] = true;
-
+            Integer l_node = d_stack.pop();
+            d_connections = d_countryNeigh.get(l_node);
+            if(!d_connections.isEmpty()){
+                for (Integer l_neighbour : d_connections) {
+                    if (!d_visited[l_neighbour]) {
+                        d_stack.push(l_neighbour);
+                        d_visited[l_neighbour] = true;
                     }
                 }
-
             }
-
-
-            count++;
+            l_count++;
         }
-
     }
 
     /**
@@ -79,36 +93,18 @@ public class GraphConnected{
      * If yes that means our graph is not connected and if no our graph is connected.
      * @return boolean variable boolean which is simple true or false.
      */
-
     public boolean ifGraphConnected() {
-
-        int l_key_1 = COUNTRYNEIGH.keySet().stream().findFirst().get();
-
-        int l_start = l_key_1;
-
-
-        dfs(l_start);
-
-        VISITED[0] = true;
-
-        for (int i = 0; i <p_RealNodes.size(); i++) {
-
-            int l_check = p_RealNodes.get(i);
-            boolean l_comp = VISITED[l_check];
-
+        int l_key_1 = d_countryNeigh.keySet().stream().findFirst().get();
+        int p_start = l_key_1;
+        dfs(p_start);
+        d_visited[0] = true;
+        for (int l_i = 0; l_i < d_realNodes.size(); l_i++) {
+            int l_check = d_realNodes.get(l_i);
+            boolean l_comp = d_visited[l_check];
             if(l_comp == false){
                 return false;
             }
-
             }
         return true;
-
-
         }
-
-
 }
-
-
-
-
