340201d8836382fed4dc537f4c43a400899b0d23
Manjit110 <49787726+Manjit110@users.noreply.github.com>
2021-03-20 20:38:07 +0530

    Add files via upload
    Abstract Class

---
+++ src/main/java/org/GamePlay/CardsAbstract.java
@@ -0,0 +1,10 @@
+package org.GamePlay;
+
+public abstract class CardsAbstract {
+
+    public void Bomb(){};
+    public void Blockade(){};
+    public void Airlift(){};
+    public void Diplomacy(){};
+
+}
