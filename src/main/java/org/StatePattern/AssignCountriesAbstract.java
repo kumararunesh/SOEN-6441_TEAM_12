ec89057b0fb2edfc0e10ccc815f4fa113a78629a
Richa Sharma <richa64044@gmail.com>
2021-03-21 13:22:00 -0400

    AssignCountries abstract class created

---
+++ src/main/java/org/StatePattern/AssignCountriesAbstract.java
@@ -0,0 +1,46 @@
+package org.StatePattern;
+
+public abstract class AssignCountriesAbstract extends Phase {
+    AssignCountriesAbstract(GameEngine1 p_ge) {
+        super(p_ge);
+    }
+
+    @Override
+    public void playGame() {
+        printInvalidCommandMessage();
+    }
+
+    @Override
+    public void startGameEngine() {
+        printInvalidCommandMessage();
+    }
+
+    @Override
+    public int playGameLoop() {
+        printInvalidCommandMessage();
+        return 0;
+    }
+
+    @Override
+    public void mainGameLoop() {
+        printInvalidCommandMessage();
+    }
+
+    @Override
+    public void EditMap() throws Exception {
+        printInvalidCommandMessage();
+    }
+
+    @Override
+    public void ShowMaps() throws Exception {
+        printInvalidCommandMessage();
+    }
+    public void execute()
+    {
+        printInvalidCommandMessage();
+    }
+    public void mapshow()
+    {
+        printInvalidCommandMessage();
+    }
+}
