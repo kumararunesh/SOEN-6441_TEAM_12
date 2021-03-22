6c6f2fdca2852102f90b7aaab8fdf98116c0485c
kumararunesh <kumararunesh211994@gmail.com>
2021-03-22 09:35:04 -0400

    Create End.java

---
+++ src/main/java/org/StatePattern/End.java
@@ -0,0 +1,58 @@
+package org.StatePattern;
+
+
+public class End extends Phase {
+
+    
+
+    @Override
+    public void playGame() {
+
+    }
+    public void assignCountries()
+    {
+
+    }
+
+    @Override
+    public void startGameEngine() {
+
+    }
+
+    @Override
+    public void execute()
+    {
+
+    }
+
+    @Override
+    public int playGameLoop() {
+        return 0;
+    }
+
+    @Override
+    public void mainGameLoop() {
+
+    }
+
+    public void mapshow()
+    {
+        printInvalidCommandMessage();
+    }
+
+    @Override
+    public void EditMap() throws Exception {
+
+    }
+
+    @Override
+    public void ShowMaps() throws Exception {
+
+    }
+
+    @Override
+    public void next()
+    {
+
+    }
+}
