6a20a77d60179c7632278646914f61226abaf56a
Manjit110 <49787726+Manjit110@users.noreply.github.com>
2021-03-24 00:04:07 +0530

    Add files via upload

--- src/test/java/org/GamePlay/BlockadeTest.java
+++ src/test/java/org/GamePlay/BlockadeTest.java
@@ -8,17 +8,24 @@ import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
 
+
+/**
+ * This is the class in which the Blockade order validity is Checked.
+ */
 public class BlockadeTest {
 
     Country d_con1 , d_con2 , d_con3;
-    Cards card1 , card2;
-    Player p1;
+    Cards d_card1 , d_card2;
+    Player d_p1;
 
+    /**
+     * Method to initialize the values required for the execute Method of the Blockade Order card.
+     */
     @Before
     public void startUpPhase(){
 
-        p1 = new Player("Manjit");
-        p1.add_card("BLOCKADE");
+        d_p1 = new Player("Manjit");
+        d_p1.add_card("BLOCKADE");
 
         d_con1 = new Country("d","b",5,3,new ArrayList<>(Arrays.asList("d","e")));
         d_con2 = new Country("e","b",5,3,new ArrayList<>(Arrays.asList("d","e")));
@@ -29,25 +36,33 @@ public class BlockadeTest {
         d_con1.d_numOfArmiesPlaced = 20;
         d_con2.d_numOfArmiesPlaced = 10;
 
-        p1.d_owned.add(d_con1);
+        d_p1.d_owned.add(d_con1);
 
-        card1 = new Cards("d" , "blockade" , d_con3);
-        card2 = new Cards("e" , "blockade" , d_con3);
+        d_card1 = new Cards("d" , "blockade" , d_con3);
+        d_card2 = new Cards("e" , "blockade" , d_con3);
 
-        p1.issue_order(card1);
-        p1.issue_order(card2);
+        d_p1.issue_order(d_card1);
+        d_p1.issue_order(d_card2);
     }
 
+    /**
+     * Method to check the execute Method from the Blockade card.
+     * In this case the player using the Blockade card successfully executes it as it uses it on its own country.
+     */
     @Test
     public void BlockadePass(){
-        card1.Execute(p1);
+        d_card1.Execute(d_p1);
 
         assertEquals(60, (int) d_con1.COUNTRIESLIST.get("d").d_numOfArmiesPlaced);
     }
 
+    /**
+     * Method to check the execute Method from the Blockade card.
+     * In this case the player using the Blockade card does not successfully executes it as it uses it on someone else's country.
+     */
     @Test
     public void BlockadeFail(){
-        card2.Execute(p1);
+        d_card2.Execute(d_p1);
 
         assertEquals(10 , (int) d_con2.COUNTRIESLIST.get("e").d_numOfArmiesPlaced);
     }
