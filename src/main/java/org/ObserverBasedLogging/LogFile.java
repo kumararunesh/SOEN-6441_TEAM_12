2a822e56afc6e5cf8953e0e7083103adb55729e0
kumararunesh <kumararunesh211994@gmail.com>
2021-03-23 08:08:48 -0400

    Update LogFile.java

--- src/main/java/org/ObserverBasedLogging/LogFile.java
+++ src/main/java/org/ObserverBasedLogging/LogFile.java
@@ -6,9 +6,15 @@ import java.util.Observer;
 public class LogFile implements Observer {
     private String d_msg;
 
-       public String getD_msg() {
+    public String getD_msg() {
         return d_msg;
     }
+
+    public void setMsg(String p_msg) throws Exception {
+        this.d_msg = p_msg;
+        LogWriter.writeLog(p_msg);
+    }
+
     @Override
     public void update(Observable o, Object msg) {
         try {
