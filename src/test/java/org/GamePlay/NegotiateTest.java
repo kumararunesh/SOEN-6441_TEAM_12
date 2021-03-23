26f941be2524ea4b22d08ff040c38ea048900d05
Manjit110 <49787726+Manjit110@users.noreply.github.com>
2021-03-24 00:05:30 +0530

    Add files via upload

--- src/test/java/org/GamePlay/NegotiateTest.java
+++ src/test/java/org/GamePlay/NegotiateTest.java
@@ -9,27 +9,34 @@ import java.util.concurrent.ConcurrentHashMap;
 
 import static org.junit.Assert.assertEquals;
 
+
+/**
+ * This is the class in which the Negotiate order validity is Checked.
+ */
 public class NegotiateTest {
 
-    Country d_con1 , d_con2 , d_con3 ,dcon4;
-    Cards card1 , card2;
-    Player p1,p2,p3;
+    Country d_con1 , d_con2 , d_con3 ;
+    Cards d_card1 , d_card2;
+    Player d_p1,d_p2,d_p3;
     ConcurrentHashMap<String,Player> d_playerList = new ConcurrentHashMap<>();
 
+    /**
+     * Method to initialize the values required for the execute Method of the Negotiate Order card.
+     */
     @Before
     public void startUpPhase(){
 
-        p1 = new Player("Manjit");
-        p1.add_card("NEGOTIATE");
+        d_p1 = new Player("Manjit");
+        d_p1.add_card("NEGOTIATE");
 
-        p2 = new Player("Manish");
-        p2.add_card("NEGOTIATE");
+        d_p2 = new Player("Manish");
+        d_p2.add_card("NEGOTIATE");
 
-        p3 = new Player("Nitpreet");
+        d_p3 = new Player("Nitpreet");
 
-        d_playerList.put("Manjit" , p1);
-        d_playerList.put("Manish" , p2);
-        d_playerList.put("Nitpreet" , p3);
+        d_playerList.put("Manjit" , d_p1);
+        d_playerList.put("Manish" , d_p2);
+        d_playerList.put("Nitpreet" , d_p3);
 
         d_con1 = new Country("d","b",5,3,new ArrayList<>(Arrays.asList("d","e","f")));
         d_con2 = new Country("e","c",5,3,new ArrayList<>(Arrays.asList("d","e","f")));
@@ -41,28 +48,36 @@ public class NegotiateTest {
         d_con1.d_numOfArmiesPlaced = 20;
         d_con2.d_numOfArmiesPlaced = 10;
 
-        p1.d_owned.add(d_con1);
-        p2.d_owned.add(d_con2);
-        p3.d_owned.add(d_con3);
+        d_p1.d_owned.add(d_con1);
+        d_p2.d_owned.add(d_con2);
+        d_p3.d_owned.add(d_con3);
 
-        card1 = new Cards(p2.d_name, "negotiate" , d_playerList);
-        card2 = new Cards(p1.d_name, "negotiate" , d_playerList);
+        d_card1 = new Cards(d_p2.d_name, "negotiate" , d_playerList);
+        d_card2 = new Cards(d_p1.d_name, "negotiate" , d_playerList);
 
-        p1.issue_order(card1);
-        p2.issue_order(card2);
+        d_p1.issue_order(d_card1);
+        d_p2.issue_order(d_card2);
     }
 
+    /**
+     * Method to check the execute Method from the Negotiate Order card.
+     * In this case the country using Negotiate card successfully executes.
+     */
     @Test
     public void NegotiatePass(){
-        card1.Execute(p1);
+        d_card1.Execute(d_p1);
 
-        assertEquals(true, p1.d_negotiate.contains(p2.d_name));
+        assertEquals(true, d_p1.d_negotiate.contains(d_p2.d_name));
     }
 
+    /**
+     * Method to check the execute Method from the Negotiate Order card.
+     * In this case the country using Negotiate card does not successfully executes.
+     */
     @Test
     public void NegotiateFail(){
-        card2.Execute(p1);
+        d_card2.Execute(d_p1);
 
-        assertEquals(false , p2.d_negotiate.contains(p3.d_name));
+        assertEquals(false , d_p2.d_negotiate.contains(d_p3.d_name));
     }
 }
