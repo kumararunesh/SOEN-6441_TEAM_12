db77226fe2714a7513345f9ccdf2ef4c22e53eca
Manjit110 <49787726+Manjit110@users.noreply.github.com>
2021-03-24 00:04:51 +0530

    Add files via upload

--- src/test/java/org/GamePlay/BombTest.java
+++ src/test/java/org/GamePlay/BombTest.java
@@ -8,17 +8,24 @@ import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
 
+
+/**
+ * This is the class in which the Bomb order validity is Checked.
+ */
 public class BombTest {
 
     Country d_con1 , d_con2 , d_con3;
-    Cards card1 , card2;
-    Player p1;
+    Cards d_card1 , d_card2;
+    Player d_p1;
 
+    /**
+     * Method to initialize the values required for the execute Method of the Bomb Order card.
+     */
     @Before
     public void startUpPhase(){
 
-        p1 = new Player("Manjit");
-        p1.add_card("BOMB");
+        d_p1 = new Player("Manjit");
+        d_p1.add_card("BOMB");
 
         d_con1 = new Country("d","b",5,3,new ArrayList<>(Arrays.asList("d","e")));
         d_con2 = new Country("e","b",5,3,new ArrayList<>(Arrays.asList("d","e")));
@@ -29,25 +36,33 @@ public class BombTest {
         d_con1.d_numOfArmiesPlaced = 20;
         d_con2.d_numOfArmiesPlaced = 10;
 
-        p1.d_owned.add(d_con1);
+        d_p1.d_owned.add(d_con1);
 
-        card1 = new Cards("e" , "bomb" , d_con3);
-        card2 = new Cards("d" , "bomb" , d_con3);
+        d_card1 = new Cards("e" , "bomb" , d_con3);
+        d_card2 = new Cards("d" , "bomb" , d_con3);
 
-        p1.issue_order(card1);
-        p1.issue_order(card2);
+        d_p1.issue_order(d_card1);
+        d_p1.issue_order(d_card2);
     }
 
+    /**
+     * Method to check the execute Method from the Bomb Order card.
+     * In this case the country using Bomb card successfully executes.
+     */
     @Test
     public void BombPass(){
-        card1.Execute(p1);
+        d_card1.Execute(d_p1);
 
         assertEquals(5, (int) d_con2.d_numOfArmiesPlaced);
     }
 
+    /**
+     * Method to check the execute Method from the Bomb Order card.
+     * In this case the country using Bomb card does not successfully executes.
+     */
     @Test
     public void BombFail(){
-        card2.Execute(p1);
+        d_card2.Execute(d_p1);
 
         assertEquals(20 , (int) d_con1.d_numOfArmiesPlaced);
     }
