d41c3d647fb075cf6e0745d178547c0a2da5b02b
Manish Sehgal <manish0718@users.noreply.github.com>
2021-03-24 09:50:59 -0400

    Update PlayerInitializationMain.java

--- src/main/java/org/StatePattern/PlayerInitializationMain.java
+++ src/main/java/org/StatePattern/PlayerInitializationMain.java
@@ -2,21 +2,36 @@ package org.StatePattern;
 
 import org.GamePlay.GameEngine;
 
+/**
+ * Main Class to initialize the Players.
+ */
 public class PlayerInitializationMain extends PlayerInitializationAbstract{
 
     GameEngine d_ge;
     GameEngine1 d_ge1;
+
+    /**
+     * Constructor to initialize the values.
+     * @param p_ge is the Object of the class GameEngine1.
+     * @param p_ge1 is the Object of the class GameEngine
+     */
     PlayerInitializationMain(GameEngine1 p_ge,GameEngine p_ge1) {
         super(p_ge);
         this.d_ge1 = p_ge;
         this.d_ge = p_ge1;
     }
 
+    /**
+     * Method to start the GameEngine.
+     */
     @Override
     public void startGameEngine() {
         d_ge.startGameEngine(d_ge.FILE);
     }
 
+    /**
+     * Method to change the Phase. If the size of the
+     */
     public void next() {
         boolean l_temp=true;
         try{
