e762785f3a6ccabe8b4bb594cfeaa38cefc65469
Manish Sehgal <manish0718@users.noreply.github.com>
2021-03-24 09:47:50 -0400

    Update ShowMap.java

--- src/main/java/org/StatePattern/ShowMap.java
+++ src/main/java/org/StatePattern/ShowMap.java
@@ -2,16 +2,36 @@ package org.StatePattern;
 
 import org.Main.Main;
 
+
+/**
+ * This is the main class of the ShowMap phase.
+ */
+
 public class ShowMap extends ShowMapAbstract {
 
+    /**
+     * This is the object of the GameEngine1 class.
+     */
     GameEngine1 d_ge;
+
+    /**
+     * This is the object of the main class.
+     */
     Main d_m;
 
+    /**
+     * This is the constructor of the ShowMap main class.
+     * @param p_ge This is the object of the GameEngine1 class.
+     */
     ShowMap(GameEngine1 p_ge) {
         super(p_ge);
         this.d_ge = p_ge;
     }
 
+    /**
+     * This method set the phase to ShowMap phase and also call the ShowMap function to print the map details.
+     * @throws Exception If file/map is not found then it will throw the exception.
+     */
     @Override
     public void ShowMaps() throws Exception {
         d_m = new Main();
@@ -20,6 +40,10 @@ public class ShowMap extends ShowMapAbstract {
 
     }
 
+    /**
+     * This method is called after the ShowMap phase.
+     * This will set the phase to MapEditor phase from ShowMap phase.
+     */
     public void next(){
         d_ge.setPhase(new MapEditor(d_ge));
     }
