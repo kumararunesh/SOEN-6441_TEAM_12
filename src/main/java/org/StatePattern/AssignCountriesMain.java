20e438b56ce01184ede06af5533ef58b44dfff3e
Manjit110 <49787726+Manjit110@users.noreply.github.com>
2021-03-24 16:48:18 +0530

    Add files via upload

--- src/main/java/org/StatePattern/AssignCountriesMain.java
+++ src/main/java/org/StatePattern/AssignCountriesMain.java
@@ -7,11 +7,22 @@ import org.GamePlay.Player;
 import java.io.File;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * Main class to assign the countries to the player.
+ */
 public class AssignCountriesMain extends AssignCountriesAbstract {
     ConcurrentHashMap<String, Player> d_playerList;
     Country d_country;
     File d_file;
     GameEngine1 d_ge1;
+
+    /**
+     * Constructor to initialize the values
+     * @param p_ge is the object of GameEngine1 class.
+     * @param p_playerList is the HashMap of the players.
+     * @param p_country is the object of the class Country.
+     * @param p_file is the file name which we want to read.
+     */
     AssignCountriesMain(GameEngine1 p_ge, ConcurrentHashMap<String, Player> p_playerList, Country p_country, File p_file) {
         super(p_ge);
         this.d_country = p_country;
@@ -19,11 +30,19 @@ public class AssignCountriesMain extends AssignCountriesAbstract {
         this.d_file = p_file;
         this.d_ge1 = p_ge;
     }
+
+    /**
+     * Method to start assigning countries to the player.
+     */
     public void assignCountries()
     {
         Assign l_a = new Assign(d_playerList, d_country);
         l_a.assignCountries(d_file);
     }
+
+    /**
+     * Method to change the phase.
+     */
     public void next()
     {
         int l_x=0;
