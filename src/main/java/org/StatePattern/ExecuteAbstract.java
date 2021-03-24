6e821459141c2e0f1d0c34d8c4459a311bfd2ec7
kumararunesh <kumararunesh211994@gmail.com>
2021-03-24 10:19:13 -0400

    Update ExecuteAbstract.java

--- src/main/java/org/StatePattern/ExecuteAbstract.java
+++ src/main/java/org/StatePattern/ExecuteAbstract.java
@@ -1,6 +1,5 @@
 package org.StatePattern;
 
-
 /**
  * Abstract class to execute the orders.
  */
@@ -20,7 +19,7 @@ public abstract class ExecuteAbstract extends Phase{
     public void playGame() {
         printInvalidCommandMessage();
     }
-    
+
     /**
      * Method to print Invalid command Error if user wants to restart the GameEngine in between the execute order phase.
      */
@@ -29,25 +28,44 @@ public abstract class ExecuteAbstract extends Phase{
 
     }
 
+    /**
+     * Method to print Invalid command Error if user enters playGame command in between the execute order phase.
+     */
+    @Override
+    public int playGameLoop() {
+        return 0;
+    }
+
+    /**
+     * Method to print Invalid command Error if user enters mainGameLoop command in between the execute order phase.
+     */
     @Override
     public void mainGameLoop() {
 
     }
 
+    /**
+     * Method to print Invalid command Error if user enters assign countries command in between the execute order phase.
+     */
     @Override
     public void assignCountries() {
-
+        printInvalidCommandMessage();
     }
 
-
+    /**
+     * Method to print Invalid command Error if user enters editMap command in between the execute order phase.
+     */
     @Override
     public void EditMap() throws Exception {
-
+        printInvalidCommandMessage();
     }
 
+    /**
+     * Method to print Invalid command Error if user wants to see the Map command in between the execute order phase.
+     */
     @Override
     public void ShowMaps() throws Exception {
-
+        printInvalidCommandMessage();
     }
 
 }
