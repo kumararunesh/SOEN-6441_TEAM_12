c7dabb92dc146df52983cc873d52eacaa848c800
Manjit110 <49787726+Manjit110@users.noreply.github.com>
2021-03-24 16:44:04 +0530

    Add files via upload

--- src/test/java/org/GamePlay/BombTest.java
+++ src/test/java/org/GamePlay/BombTest.java
@@ -16,7 +16,7 @@ public class BombTest {
 
     Country d_con1 , d_con2 , d_con3;
     Cards d_card1 , d_card2;
-    Player d_p1;
+    Player d_p1,d_p2;
 
     /**
      * Method to initialize the values required for the execute Method of the Bomb Order card.
@@ -26,6 +26,8 @@ public class BombTest {
 
         d_p1 = new Player("Manjit");
         d_p1.add_card("BOMB");
+        d_p2 = new Player("Manish");
+        d_p2.add_card("BOMB");
 
         d_con1 = new Country("d","b",5,3,new ArrayList<>(Arrays.asList("d","e")));
         d_con2 = new Country("e","b",5,3,new ArrayList<>(Arrays.asList("d","e")));
@@ -37,12 +39,13 @@ public class BombTest {
         d_con2.d_numOfArmiesPlaced = 10;
 
         d_p1.d_owned.add(d_con1);
+        d_p2.d_owned.add(d_con2);
 
         d_card1 = new Cards("e" , "bomb" , d_con3);
-        d_card2 = new Cards("d" , "bomb" , d_con3);
+        d_card2 = new Cards("e" , "bomb" , d_con3);
 
         d_p1.issue_order(d_card1);
-        d_p1.issue_order(d_card2);
+        d_p2.issue_order(d_card2);
     }
 
     /**
@@ -53,7 +56,7 @@ public class BombTest {
     public void BombPass(){
         d_card1.Execute(d_p1);
 
-        assertEquals(5, (int) d_con2.d_numOfArmiesPlaced);
+        assertEquals(5, (int) d_con2.COUNTRIESLIST.get("e").d_numOfArmiesPlaced);
     }
 
     /**
@@ -62,8 +65,8 @@ public class BombTest {
      */
     @Test
     public void BombFail(){
-        d_card2.Execute(d_p1);
+        d_card2.Execute(d_p2);
 
-        assertEquals(20 , (int) d_con1.d_numOfArmiesPlaced);
+        assertEquals(10 , (int) d_con2.COUNTRIESLIST.get("e").d_numOfArmiesPlaced);
     }
 }
