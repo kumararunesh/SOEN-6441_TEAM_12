a79759a8545c2b5ef333e093aedead04eed0f82d
Manjit110 <49787726+Manjit110@users.noreply.github.com>
2021-03-25 16:53:39 +0530

    Add files via upload

--- src/main/java/org/GamePlay/CardsAbstract.java
+++ src/main/java/org/GamePlay/CardsAbstract.java
@@ -1,10 +1,25 @@
 package org.GamePlay;
 
+/**
+ * This is the Abstract class which contains the declaration of Card Order Function.
+ */
 public abstract class CardsAbstract {
 
+    /**
+     * The function implements the functionality of Bomb card
+     */
     public void Bomb(){};
+    /**
+     * The function implements the functionality of Blockade card
+     */
     public void Blockade(){};
+    /**
+     * The function implements the functionality of Airlift card
+     */
     public void Airlift(){};
+    /**
+     * The function implements the functionality of Diplomacy card
+     */
     public void Diplomacy(){};
 
 }
