bae8be2e130379f350c213a0f1b84b2f6533e767
Manish Sehgal <manish0718@users.noreply.github.com>
2021-04-14 19:09:10 -0400

    Update Main.java

--- src/main/java/org/Main/Main.java
+++ src/main/java/org/Main/Main.java
@@ -3,13 +3,12 @@ package org.Main;
 import java.io.File;
 import java.util.*;
 
-import org.GamePlay.GameEngine.*;
-
 /***
  * This class is the Main file from where the game starts.
  */
 
 public class Main {
+
 
     /**
      * FILE is the object which is used to store the path of the map.
@@ -26,7 +25,10 @@ public class Main {
      * If file doesn't exixts then it will ask the user to create a new map, else it will allow user to interact with the map and edit it.
      */
     public static void EditMap() {
+        EditMap editmap = new EditMap();
+        EditMapConquest editmaps = new EditMapConquest();
         int l_flag = 0;
+        int l_flag9 = 0;
         while (true) {
             System.out.println("Enter edit filename command to edit map");
             SC = new Scanner(System.in);
@@ -36,7 +38,7 @@ public class Main {
                 try {
                     if (l_text[0].equalsIgnoreCase("edit")) {
                         String l_file = null;
-                        File l_directoryPath = new File("SOEN-6441_TEAM_12-main\\src\\main\\resources\\maps");
+                        File l_directoryPath = new File("C:\\SOEN-6441_TEAM_12-main\\src\\main\\resources\\maps\\");
                         String l_contents[] = l_directoryPath.list();
                         for (int l_i = 0; l_i < l_contents.length; l_i++) {
                             if (l_text[1].equals(l_contents[l_i])) {
@@ -52,8 +54,29 @@ public class Main {
                         }
 
                         if (l_flag == 1) {
-                            EditMap l_edit = new EditMap();
-                            l_edit.editMap(l_file);
+                            File file = new File(l_directoryPath +"\\" +l_file);
+                            Scanner l_sc3 = new Scanner(file);
+                            while (l_sc3.hasNextLine()) {
+                                String x =  l_sc3.next();
+                                if (x.equals("[countries]")) {
+                                    l_flag9 = 1;
+                                    break;
+                                }
+                                if (x.equals("[Territories]")) {
+                                    l_flag9 = 2;
+                                    break;
+                                }
+                            }
+
+                            if(l_flag9 == 1){
+                                EditDominationAdapter edit = new EditDominationAdapter(editmap);
+                                edit.editMap(l_file);
+                            }
+
+                            if(l_flag9 == 2){
+                                EditConquestAdapter edits = new EditConquestAdapter(editmaps);
+                                edits.editMap(l_file);
+                            }
                         }
                         break;
                     } else {
@@ -78,9 +101,28 @@ public class Main {
      */
 
     public static void createMapByUser(File p_file1) throws Exception {
-        FILE = new File("SOEN-6441_TEAM_12-main\\src\\main\\resources\\maps\\" + p_file1);
-        MapCreate l_create = new MapCreate();
-        l_create.createMap(FILE);
+        FILE = new File("C:\\SOEN-6441_TEAM_12-main\\src\\main\\resources\\maps\\" + p_file1);
+        while (true) {
+            System.out.println("In which format you want to save the file");
+            System.out.println("1. Domination");
+            System.out.println("2. Conquest");
+            int l_ans = SC.nextInt();
+            switch (l_ans) {
+                case 1:
+                    MapCreate l_map = new MapCreate();
+                    l_map.createMap(FILE);
+                    break;
+
+                case 2:
+                    MapCreateConquest l_maps = new MapCreateConquest();
+                    l_maps.createMap(FILE);
+                    break;
+
+                default:
+                    System.out.println("Enter correct command");
+
+            }
+        }
     }
 
     /**
@@ -89,7 +131,79 @@ public class Main {
      * @throws Exception If file does not found at the path mentioned at ShowMap file, this will throw exception.
      */
     public static void mapShow() throws Exception {
-        ShowMap l_show = new ShowMap();
-        l_show.mapshow();
+        int l_flag = 0;
+        while (true) {
+            try {
+                Scanner l_sc = new Scanner(System.in);
+                File l_directoryPath = new File("C:\\SOEN-6441_TEAM_12-main\\src\\main\\resources\\maps\\");
+                String l_contents[] = l_directoryPath.list();
+                int l_flag5 = 0;
+                if (l_contents.length == 0) {
+                    System.out.println("There are no predefined maps");
+                    System.out.println("Let's create a new map");
+                    while (true) {
+                        System.out.println("Enter the name of the map");
+                        SC = new Scanner(System.in);
+                        String l_filename = SC.nextLine();
+                        if (l_filename.endsWith(".map")) {
+                            try {
+                                File newFile = new File(l_filename);
+                                l_flag5 = 1;
+                                createMapByUser(newFile);
+                                break;
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        } else {
+                            System.out.println("Enter correct command\n");
+                        }
+                    }
+                    if (l_flag5 == 1) {
+                        break;
+                    }
+                }
+
+                System.out.println("Here are the list of maps");
+                System.out.println("");
+                System.out.println("*************************");
+                for (int l_i = 0; l_i < l_contents.length; l_i++) {
+                    System.out.println(l_contents[l_i]);
+                }
+                System.out.println("*************************");
+                System.out.println("");
+                System.out.println("Enter the name of the map you want to select (Don't use extension)");
+                String l_x = l_sc.nextLine();
+                FILE = new File("C:\\SOEN-6441_TEAM_12-main\\src\\main\\resources\\maps\\" + l_x + ".map");
+                break;
+            } catch (Exception e) {
+                System.out.println("Enter valid map name");
+            }
+        }
+        ShowMapConquest l_show = new ShowMapConquest();
+        ShowMap l_map = new ShowMap();
+
+        Scanner l_sc3 = new Scanner(FILE);
+        while (l_sc3.hasNextLine()) {
+            String x =  l_sc3.next();
+            if (x.equals("[countries]")) {
+                l_flag = 1;
+                break;
+            }
+            if (x.equals("[Territories]")) {
+                l_flag = 2;
+                break;
+            }
+        }
+
+        if (l_flag == 1) {
+            DominationAdapter l_maps = new DominationAdapter(l_map);
+            l_maps.mapshow(FILE);
+        }
+
+        if(l_flag == 2){
+            ConquestAdapter l_maps = new ConquestAdapter(l_show);
+            l_maps.mapshow(FILE);
+        }
     }
+
 }
