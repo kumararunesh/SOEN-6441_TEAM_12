58080e5f8c10b859c046a195d082592a106995e9
Manjit110 <49787726+Manjit110@users.noreply.github.com>
2021-04-15 16:53:41 +0530

    Add files via upload

--- src/main/java/org/StrategyPattern/AggressivePlayerStrategy.java
+++ src/main/java/org/StrategyPattern/AggressivePlayerStrategy.java
@@ -5,68 +5,107 @@ import org.GamePlay.*;
 import java.util.Random;
 import java.util.concurrent.ConcurrentHashMap;
 
+
+/**
+ * Aggressive player strategy class
+ */
 public class AggressivePlayerStrategy extends PlayerStrategy{
 
-    Country country;
-    public AggressivePlayerStrategy(Player p_player, ConcurrentHashMap<String, Country> Countries, Country country){
-        super(p_player, Countries);
-        this.country=country;
+    Country d_country;
+
+    /**
+     * Aggressive strategy constructor being initialized
+     * @param p_player Player class object
+     * @param p_Countries It is a hashmap which has a key as country name and value as object of its key.
+     * @param p_country Country class object
+     */
+    public AggressivePlayerStrategy(Player p_player, ConcurrentHashMap<String, Country> p_Countries, Country p_country){
+        super(p_player, p_Countries);
+        this.d_country=p_country;
     }
 
+    /**
+     *	Determines the country to attack/move to.
+     *	The Aggressive player determines this randomly
+     *	@return random territory
+     */
     @Override
     protected Country toAttack() {
-        int num=0;
-        Country attack =null;
+        int l_num=0;
+        Country l_attack =null;
         for(Country c :d_player.d_owned){
             for(String n: c.d_neighbours){
-                if(num>=country.COUNTRIESLIST.get(n).d_numOfArmiesPlaced && d_player.d_owned.contains(country.COUNTRIESLIST.get(n))){
-                    attack = country.COUNTRIESLIST.get(n);
-                    num = country.COUNTRIESLIST.get(n).d_numOfArmiesPlaced;
+                if(l_num>=d_country.COUNTRIESLIST.get(n).d_numOfArmiesPlaced && d_player.d_owned.contains(d_country.COUNTRIESLIST.get(n))){
+                    l_attack = d_country.COUNTRIESLIST.get(n);
+                    l_num = d_country.COUNTRIESLIST.get(n).d_numOfArmiesPlaced;
                 }
             }
         }
-        if(attack==null){
-            attack = country.COUNTRIESLIST.get(country.COUNTRIESLIST.keySet().stream().findFirst().get());
+        if(l_attack==null){
+            l_attack = d_country.COUNTRIESLIST.get(d_country.COUNTRIESLIST.keySet().stream().findFirst().get());
         }
-        return attack;
+        return l_attack;
     }
 
+
+    /**
+     *	Determines the country to attack from which country.
+     *	The Aggressive player determines this randomly.
+     *	@return null
+     */
     @Override
     protected Country toAttackFrom() {
         return null;
     }
 
+
+    /**
+     *	Determines the country to move from.
+     *	The Aggressive player does not use this, so it returns null.
+     *	@return null
+     */
     @Override
     protected Country toMoveFrom() {
         return null;
     }
 
+
+    /**
+     *	Determines the country to attack from.
+     *	The Aggressive player does not use this, so it returns null.
+     *	@return null
+     */
     @Override
     protected Country toDefend() {
-        int num = 0;
-        Country defend = null;
-        for(Country c : d_player.d_owned){
-            if( num <= c.d_numOfArmiesPlaced){
-                defend = c;
-                num = c.d_numOfArmiesPlaced;
+        int l_num = 0;
+        Country l_defend = null;
+        for(Country l_c : d_player.d_owned){
+            if( l_num <= l_c.d_numOfArmiesPlaced){
+                l_defend = l_c;
+                l_num = l_c.d_numOfArmiesPlaced;
             }
         }
-        return defend;
+        return l_defend;
     }
 
+    /**
+     *	Creates and order.
+     *	The Aggressive player can either deploy or advance, determined randomly. .
+     *	@return Order class object
+     */
     @Override
     public Order createOrder() {
-        Random rand = new Random();
-        int rnd_num_of_armies = rand.nextInt(d_player.d_armiesNum);
-        if (rand.nextInt(5) != 0) {
-            switch (rand.nextInt(2)) {
+        Random l_rand = new Random();
+        int l_rnd_num_of_armies = l_rand.nextInt(d_player.d_armiesNum);
+        if (l_rand.nextInt(5) != 0) {
+            switch (l_rand.nextInt(2)) {
                 case (0):
-                    return new DeployOrder(d_player.getD_name(), rnd_num_of_armies, toDefend());
+                    return new DeployOrder(d_player.getD_name(), l_rnd_num_of_armies, toDefend());
 
                 case (1):
-                    return new AdvanceArmies(toDefend().d_countryId, toAttack().d_countryId, rnd_num_of_armies, country, "advance");
+                    return new AdvanceArmies(toDefend().d_countryId, toAttack().d_countryId, l_rnd_num_of_armies, d_country, "advance");
             }
         }
-        return new AdvanceArmies(toDefend().d_countryId, toAttack().d_countryId, rnd_num_of_armies, country, "advance");
+        return new AdvanceArmies(toDefend().d_countryId, toAttack().d_countryId, l_rnd_num_of_armies, d_country, "advance");
     }
 }
--- src/main/java/org/StrategyPattern/RandomPlayerStrategy.java
+++ src/main/java/org/StrategyPattern/RandomPlayerStrategy.java
@@ -9,11 +9,17 @@ import java.util.concurrent.ConcurrentHashMap;
  *	ConcreteStrategy of the Strategy pattern
  */
 public class RandomPlayerStrategy extends PlayerStrategy {
-	Country country;
+	Country d_country;
 
-	public RandomPlayerStrategy(Player p_player, ConcurrentHashMap<String, Country> p_map , Country country) {
+	/**
+	 * Random Player Strategy behaviour
+	 * @param p_player Player class object
+	 * @param p_map Countries Hashmap. Key country name and value object of that country
+	 * @param p_country Country class object
+	 */
+	public RandomPlayerStrategy(Player p_player, ConcurrentHashMap<String, Country> p_map , Country p_country) {
 		super(p_player, p_map);
-		this.country = country;
+		this.d_country = p_country;
 	}
 
 	/**
@@ -22,10 +28,10 @@ public class RandomPlayerStrategy extends PlayerStrategy {
 	 *	@return random territory
 	 */
 	protected Country toAttack() {
-		Random generator = new Random();
-		Object[] values = (Object[]) d_map.values().toArray();
-		Country rnd_target_territory = (Country) values[generator.nextInt(values.length+0)];
-		return rnd_target_territory;
+		Random l_generator = new Random();
+		Object[] l_values = (Object[]) d_map.values().toArray();
+		Country l_rnd_target_territory = (Country) l_values[l_generator.nextInt(l_values.length+0)];
+		return l_rnd_target_territory;
 	}
 	
 	/**
@@ -34,10 +40,10 @@ public class RandomPlayerStrategy extends PlayerStrategy {
 	 *	@return random territory owned by the player
 	 */
 	protected Country toDefend() {
-		Random rand = new Random();
-		int rnd_target_territory;
-		rnd_target_territory = rand.nextInt(d_player.d_owned.size()+0);
-		return(d_player.d_owned.get(rnd_target_territory));
+		Random l_rand = new Random();
+		int l_rnd_target_territory;
+		l_rnd_target_territory = l_rand.nextInt(d_player.d_owned.size()+0);
+		return(d_player.d_owned.get(l_rnd_target_territory));
 	}
 
 	/**
@@ -64,34 +70,34 @@ public class RandomPlayerStrategy extends PlayerStrategy {
 	 *	@return null
 	 */
 	public Order createOrder() {
-		Random rand = new Random();
+		Random l_rand = new Random();
 
-		int rnd_num_of_armies = rand.nextInt(d_player.d_armiesNum-1);
-		if (rand.nextInt(5) != 0) {
-			switch (rand.nextInt(3)) {
+		int rnd_num_of_armies = l_rand.nextInt(d_player.d_armiesNum-1);
+		if (l_rand.nextInt(5) != 0) {
+			switch (l_rand.nextInt(3)) {
 				case (0):
 					return new DeployOrder(d_player.getD_name(), rnd_num_of_armies, toDefend());
 
 				case (1):
 					if(d_player.d_cards.contains("AIRLIFT")){
-						return new AdvanceArmies(toDefend().d_countryId, toDefend().d_countryId, rnd_num_of_armies, country, "airlift");
+						return new AdvanceArmies(toDefend().d_countryId, toDefend().d_countryId, rnd_num_of_armies, d_country, "airlift");
 					}
 					else {
-						return new AdvanceArmies(toDefend().d_countryId, toDefend().d_countryId, rnd_num_of_armies, country, "advance");
+						return new AdvanceArmies(toDefend().d_countryId, toDefend().d_countryId, rnd_num_of_armies, d_country, "advance");
 					}
 
 				case (2):
 					if(d_player.d_cards.contains("BOMB")){
-						return new Cards(toAttack().d_countryId,"bomb",country);
+						return new Cards(toAttack().d_countryId,"bomb",d_country);
 					}
 					else if(d_player.d_cards.contains("BLOCKADE")){
-						return new Cards(toAttack().d_countryId,"blockade",country);
+						return new Cards(toAttack().d_countryId,"blockade",d_country);
 					}
 					else {
-						return new AdvanceArmies(toDefend().d_countryId, toDefend().d_countryId, rnd_num_of_armies, country, "advance");
+						return new AdvanceArmies(toDefend().d_countryId, toDefend().d_countryId, rnd_num_of_armies, d_country, "advance");
 					}
 			}
 		}
-		return new AdvanceArmies(toDefend().d_countryId, toDefend().d_countryId, rnd_num_of_armies, country, "advance");
+		return new AdvanceArmies(toDefend().d_countryId, toDefend().d_countryId, rnd_num_of_armies, d_country, "advance");
 	}
 }
