3244f7ceafefe539a5f98f951b4b4aacea4df5ef
kumararunesh <kumararunesh211994@gmail.com>
2021-04-14 21:58:21 -0400

    Addding Player Behaviors

--- src/main/java/org/GamePlay/TournamentGameEngine.java
+++ src/main/java/org/GamePlay/TournamentGameEngine.java
@@ -1,2 +1,14 @@
 public class TournamentGameEngine {
+  /**
+     * GLobal List of Players
+     */
+    public static ConcurrentHashMap<String, Player> PLAYERS_LIST = new ConcurrentHashMap<String, Player>(); // concurrent only - thread safe .
+    public static ArrayList<String> Behaviours = new ArrayList<String>();
+    public void set_Behaviour(){
+        Behaviours.add("random");
+        Behaviours.add("human");
+        Behaviours.add("aggressive");
+        Behaviours.add("cheater");
+        Behaviours.add("benevolent");
+    }
 }
