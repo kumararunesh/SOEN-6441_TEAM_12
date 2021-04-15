94e78736d01552a2241848faa8fc40b2e3ee52e8
Manjit110 <49787726+Manjit110@users.noreply.github.com>
2021-04-15 17:07:15 +0530

    Add files via upload

--- src/main/java/org/Tournament/TournamentMain.java
+++ src/main/java/org/Tournament/TournamentMain.java
@@ -9,48 +9,55 @@ import java.util.Map;
 import java.util.Scanner;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * Tournament mode class
+ */
 public class TournamentMain {
 
-    String listOfMapFiles[] ;
-    public  ConcurrentHashMap<String, Player> PLAYERS_LIST = new ConcurrentHashMap<String, Player>(); // concurrent only - thread safe .
-    public  ConcurrentHashMap<String, Country> COUNTRIES = new ConcurrentHashMap<>();
-    public HashMap<String,String> gameState = new HashMap<>();
+    String d_listOfMapFiles[] ;
+    public  ConcurrentHashMap<String, Player> d_PLAYERS_LIST = new ConcurrentHashMap<String, Player>(); // concurrent only - thread safe .
+    public  ConcurrentHashMap<String, Country> d_COUNTRIES = new ConcurrentHashMap<>();
+    public HashMap<String,String> d_gameState = new HashMap<>();
     Country d_country;
     File d_file;
-    public  int numberofgames , j =0;
-    public  int maxnumberofturns , k =0;
-    public  int noOfMap , i =0;
-    static int count ;
-    static String winningPlayer ;
+    public  int d_numberofgames , j =0;
+    public  int d_maxnumberofturns , k =0;
+    public  int d_noOfMap , i =0;
+    static int d_count ;
+    static String d_winningPlayer ;
     Assign d_assign;
-    TournamentGameEngine gameEngine ;
-    public playGame play;
-    public ArrayList<String> winners = new ArrayList<String>();
+    TournamentGameEngine d_gameEngine ;
+    public playGame d_play;
+    public ArrayList<String> d_winners = new ArrayList<String>();
 
 
     String[] d_playersName;
-    String winner;
+    String D_winner;
 
+    /**
+     * This is from where tournament mode starts
+     * @return runs overloaded run function
+     */
     public String run() {
         System.out.println("Enter command to start the tournament mode");
-        Scanner sc = new Scanner(System.in);
-        String l_command = sc.nextLine();
+        Scanner l_sc = new Scanner(System.in);
+        String l_command = l_sc.nextLine();
         return run(l_command);
     }
 
     public String run(String l_command){
-        gameEngine = new TournamentGameEngine();
+        d_gameEngine = new TournamentGameEngine();
         String[] l_commandSplit= l_command.split("-");
         if(l_commandSplit[0].equalsIgnoreCase("tournament ") && l_commandSplit.length==5)
         {
-            listOfMapFiles = new String[l_commandSplit[1].split(" ").length-1];
+            d_listOfMapFiles = new String[l_commandSplit[1].split(" ").length-1];
             int i=0;
             if(l_commandSplit[1].split(" ")[0].equalsIgnoreCase("m")) {
                 for (String map : l_commandSplit[1].split(" ")) {
                     if (map.equalsIgnoreCase("m")) {
                         continue;
                     } else {
-                        listOfMapFiles[i] = map;
+                        d_listOfMapFiles[i] = map;
                         i += 1;
                     }
                 }
@@ -84,7 +91,7 @@ public class TournamentMain {
 
             if(l_commandSplit[3].split(" ")[0].equalsIgnoreCase("g"))
             {
-                numberofgames= Integer.parseInt(l_commandSplit[3].split(" ")[1]);
+                d_numberofgames= Integer.parseInt(l_commandSplit[3].split(" ")[1]);
 
             }
             else
@@ -95,7 +102,7 @@ public class TournamentMain {
 
             if(l_commandSplit[4].split(" ")[0].equalsIgnoreCase("d"))
             {
-                maxnumberofturns = Integer.parseInt(l_commandSplit[4].split(" ")[1]);
+                d_maxnumberofturns = Integer.parseInt(l_commandSplit[4].split(" ")[1]);
             }
             else
             {
@@ -109,30 +116,30 @@ public class TournamentMain {
             return "Wrong command";
         }
 
-        for (int i = 0; i < listOfMapFiles.length; i++) {
-            System.out.println("Map" + listOfMapFiles[i] + " : ");
-            d_file = new File("src/main/resources/maps/" + listOfMapFiles[i]);
+        for (int i = 0; i < d_listOfMapFiles.length; i++) {
+            System.out.println("Map" + d_listOfMapFiles[i] + " : ");
+            d_file = new File("src/main/resources/maps/" + d_listOfMapFiles[i]);
 
 
-            for (int j = 0; j < numberofgames; j++) {
+            for (int j = 0; j < d_numberofgames; j++) {
                 for(int t = 0 ; t<d_playersName.length -1 ; t++)
                 {
-                    PLAYERS_LIST.put(d_playersName[t], new Player(d_playersName[t]));
+                    d_PLAYERS_LIST.put(d_playersName[t], new Player(d_playersName[t]));
                 }
 
-                d_assign = new Assign(PLAYERS_LIST,d_country);
+                d_assign = new Assign(d_PLAYERS_LIST,d_country);
                 d_assign.assignCountries(d_file);
-                COUNTRIES = d_assign.d_country.COUNTRIESLIST;
+                d_COUNTRIES = d_assign.d_country.COUNTRIESLIST;
                 d_country = d_assign.d_country;
 
-                play = new playGame(PLAYERS_LIST,d_country);
-                play.playGameLoop();
+                d_play = new playGame(d_PLAYERS_LIST,d_country);
+                d_play.playGameLoop();
                 int k = j+1;
-                gameState = gameEngine.gamePlay(d_file,maxnumberofturns,PLAYERS_LIST,d_country,listOfMapFiles[i],gameState);
-                winners.add("Winner of game " + k +" on map " + listOfMapFiles[i] + " is " +gameState.get(listOfMapFiles[i]) +"\n");
+                d_gameState = d_gameEngine.gamePlay(d_file,d_maxnumberofturns,d_PLAYERS_LIST,d_country,d_listOfMapFiles[i],d_gameState);
+                d_winners.add("Winner of game " + k +" on map " + d_listOfMapFiles[i] + " is " +d_gameState.get(d_listOfMapFiles[i]) +"\n");
             }
 
-            System.out.println(winners + "\n");
+            System.out.println(d_winners + "\n");
         }
         return "Tournament Over";
     }
