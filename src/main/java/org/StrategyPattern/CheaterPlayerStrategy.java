3e59268d8d2021a00bf1caae899288f376e29dd7
Nitpreet Taneja <32680124+Nitpreet@users.noreply.github.com>
2021-04-15 16:18:30 +0530

    Update CheaterPlayerStrategy.java

--- src/main/java/org/StrategyPattern/CheaterPlayerStrategy.java
+++ src/main/java/org/StrategyPattern/CheaterPlayerStrategy.java
@@ -1,1 +1,23 @@
+package org.StrategyPattern;
 
+import org.GamePlay.Country;
+import org.GamePlay.Order;
+import org.GamePlay.Player;
+
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class CheaterPlayerStrategy extends PlayerStrategy {
+
+    ConcurrentHashMap <String,Country> d_countries;
+    Player d_player;
+    ConcurrentHashMap<String,Player> d_playerList;
+
+    public CheaterPlayerStrategy(Player p_player, ConcurrentHashMap<String, Country> Countries, ConcurrentHashMap<String, Player> p_playerList )
+    {
+        super(p_player, Countries);
+        this.d_countries = Countries;
+        this.d_player = p_player;
+        this.d_playerList = p_playerList;
+    }
+}
